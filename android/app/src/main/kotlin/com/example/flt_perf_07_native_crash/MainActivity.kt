2103bad5ebf080c9faa3db989db7ed3eaa211a3c
Sebastian Roth <sebastian@nextbase.co.uk>
2019-12-06 14:34:33 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/flt_perf_07_native_crash/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.flt_perf_07_native_crash
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
